7d955ef2868955f9f921ef6e3903ada60723d522
ThomasDev4 <tl.lemallier@gmail.com>
2024-11-08 10:20:56 +0100

    update of the home page

---
+++ client/src/components/homepage/footer/index.tsx
@@ -0,0 +1,6 @@
+import "./index.css"
+export function Footer () {
+    <footer> 
+        <p>Made with love ❤️ from the good guys</p>
+    </footer>
+}
---
+++ client/src/components/homepage/homepage.tsx
@@ -0,0 +1,12 @@
+import { NavBar } from "./navbar"
+import { Main } from "./main"
+import { Footer } from "./footer"
+export function HomePage () {
+    return ( 
+        <>
+        <NavBar/>
+        <Main/>
+        <Footer/>
+      </>
+    )
+}
