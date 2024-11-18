91b44b530d9b60a6f4415f42dd973559ca9ba08e
PIERRE DE CASTELBAJAC <decastelbajacpierre@gmail.com>
2024-11-18 11:55:34 +0100

    menu burger and css

--- client/src/components/homepage/navbar/index.tsx
+++ client/src/components/homepage/navbar/index.tsx
@@ -1,28 +1,61 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./index.css";
 
 export function NavBar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header>
       <nav>
-        <ul className="links">
-          <li className="link">
-            <Link to="/">
-              <img src="./public/logo-smash.png" alt="accueil" />
+        <button
+          className={`burger ${isMenuOpen ? "active" : ""}`}
+          onClick={toggleMenu}
+          type="button"
+        >
+          <button
+            className={`line ${isMenuOpen ? "active" : ""}`}
+            type="button"
+          />
+          <button
+            className={`line ${isMenuOpen ? "active" : ""}`}
+            type="button"
+          />
+          <button
+            className={`line ${isMenuOpen ? "active" : ""}`}
+            type="button"
+          />
+        </button>
+        <Link to="/" onClick={() => setIsMenuOpen(false)}>
+          <h1>Smash Cards</h1>
+        </Link>
+        <ul className={`link ${isMenuOpen ? "active" : ""}`}>
+          <li>
+            <Link to="/" onClick={() => setIsMenuOpen(false)}>
+              <img src="/logo-smash.png" alt="Accueil" />
             </Link>
           </li>
-          <li className="link">
-            <Link to="/scorepage">Score</Link>
+          <li>
+            <Link to="/scorepage" onClick={() => setIsMenuOpen(false)}>
+              Score
+            </Link>
           </li>
-          <li className="link">
-            <Link to="/description">Description</Link>
+          <li>
+            <Link to="/description" onClick={() => setIsMenuOpen(false)}>
+              Description
+            </Link>
           </li>
-          <li className="link">
-            <Link to="/About">About</Link>
+          <li>
+            <Link to="/about" onClick={() => setIsMenuOpen(false)}>
+              About
+            </Link>
           </li>
         </ul>
       </nav>
-      <h1>Smash Cards</h1>
     </header>
   );
 }
